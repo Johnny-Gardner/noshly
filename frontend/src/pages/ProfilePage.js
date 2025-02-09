23b68b56a08a04daadf045bffd1825723c86d230
Johnny-Gardner <johnnygardner@outlook.com>
2025-02-09 19:07:24 +0000

    Updated babel plugin

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import { useNavigate, useOutletContext  } from "react-router-dom";
 import "./ProfilePage.css"; // Profile-specific styles
